37811b813833417e5a5cbc45664198a92023d50b
sebastian carrero <mugssebastian@gmail.com>
2022-12-01 19:17:03 -0500

    enlazando componentes, creando ingresarPresupuesto

--- gestor1/src/app/app-routing.module.ts
+++ gestor1/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+//componentes
+import { IngresarPresupuestoComponent } from './componentes/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresosComponent } from './componentes/ingresos/ingresos.component';
+import { GastosComponent } from './componentes/gastos/gastos.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'},
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent},
+  { path: 'gastos', component: GastosComponent},
+  { path: 'ingresos', component: IngresosComponent},
+  { path: '**', redirectTo: '/ingresarPresupuesto', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
